29a56978ff9f85d3d70a11827b4134bd597e284f
Rick Reinhard <rar15@my.fsu.edu>
2020-10-22 01:54:38 -0400

    first push

---
+++ portfolio/code/js/script.js
@@ -0,0 +1,11 @@
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+function myForm() {
+    document.getElementById("frm1").submit();
+}
